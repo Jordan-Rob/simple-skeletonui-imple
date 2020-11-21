b2d6dad93afbaafb31bd9ffd02299b9b4240c595
jordanrob709 <jordanrob709@gmail.com>
2020-11-21 18:20:56 +0300

    created skeleton component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import CardList from './components/CardList'
 import dummyData from "./data";
+import SkeletonCard from "./components/SkeletonCard";
+
 
 const App = () => {
   const [videos, setVideos] = useState([]);
@@ -14,6 +16,8 @@ const App = () => {
     }, 5000);
     return () => clearTimeout(timer);
   }, []);
+
+  /*
   return (
     <div className="App">
       {
@@ -28,5 +32,23 @@ const App = () => {
         })}
     </div>
   );
+  */
+
+ return (
+  <div className="App">
+    {loading && <SkeletonCard />}
+    {!loading &&
+      videos.map((list, index) => {
+        return (
+          <section key={index}>
+            <h2 className="section-title">{list.section}</h2>
+            <CardList list={list} />
+            <hr />
+          </section>
+        );
+      })}
+  </div>
+);
+
 };
 export default App;
